2758ba2dbd8d42a0a16084ddcffcd04740062c2f
nu0ma <ishikawaryoufutoshi@ishikawaryoufutoshinoMacBook-Air.local>
2021-10-03 11:38:46 +0900

    [#4] fix: change schema file extension

--- lib/apiClient.ts
+++
@@ -1,7 +0,0 @@
-import api from '../api/$api';
-import aspida from '@aspida/axios';
-import axios from 'axios';
-
-const axiosConfig = { baseURL: 'http://localhost:3000' };
-
-export const apiClient = api(aspida(axios, axiosConfig));
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,7 @@
 import type { NextPage } from 'next';
-import { useEffect, useState } from 'react';
-import { apiClient } from '../lib/apiClient';
 
 const Home: NextPage = () => {
-  const [posts, setPosts] = useState<
-    { id: number; title: string; author: string }[]
-  >([]);
-
-  useEffect(() => {
-    const fetchPost = async () => {
-      const res = await apiClient.posts.get({
-        query: {
-          id: '1',
-        },
-      });
-      setPosts(res.body);
-    };
-    fetchPost();
-  }, []);
-
-  return <div>{JSON.stringify(posts)}</div>;
+  return <div>Home</div>;
 };
 
 export default Home;
