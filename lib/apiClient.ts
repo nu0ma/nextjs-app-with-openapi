d80096f119a707f8c75fb1b28ae3ea5fa9e90fa9
nu0ma <ishikawaryoufutoshi@ishikawaryoufutoshinoMacBook-Air.local>
2021-10-04 21:20:59 +0900

    [#11] feat: create usePost test

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -2,6 +2,4 @@ import api from '../api/$api';
 import aspida from '@aspida/axios';
 import axios from 'axios';
 
-const axiosConfig = { baseURL: 'http://localhost:3000' };
-
-export const apiClient = api(aspida(axios, axiosConfig));
+export const apiClient = api(aspida(axios));
