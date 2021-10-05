243df50e314c7025d905fb17dc26254ae6ebd62d
nu0ma <ishikawaryoufutoshi@ishikawaryoufutoshinoMacBook-Air.local>
2021-10-05 23:26:34 +0900

    [#17] fix: fix fetcher at error

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,1 +1,14 @@
-export const fetcher = (url: string) => fetch(url).then((r) => r.json());
+export const fetcher = async (url: string) => {
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    const error = new Error('An error occurred while fetching the data.');
+    // @ts-ignore
+    error.info = await res.json();
+    // @ts-ignore
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
