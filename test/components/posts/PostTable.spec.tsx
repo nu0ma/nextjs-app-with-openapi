cdb22c46880444138b583dfabf0a6f3d7cd2f99c
nu0ma <ishikawaryoufutoshi@ishikawaryoufutoshinoMacBook-Air.local>
2021-10-08 00:27:04 +0900

    [#21] test: add post table component test

---
+++ test/components/posts/PostTable.spec.tsx
@@ -0,0 +1,48 @@
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+import { PostTable } from '@/components/posts/PostTable';
+import components from '@/mocks/components';
+
+jest.mock('swr');
+
+describe('Post Table', () => {
+  beforeEach(() => {
+    render(<PostTable posts={components.Posts} />);
+  });
+
+  test('propsで与えたPostsが正常に表示される', async () => {
+    const id = screen.getByText(components.Posts[0].id);
+    const title = screen.getByText(components.Posts[0].title);
+    const author = screen.getByText(components.Posts[0].author);
+    expect(id).not.toBe(null);
+    expect(title).not.toBe(null);
+    expect(author).not.toBe(null);
+  });
+
+  test('記事の編集ができる', async () => {
+    const newTitle = 'updated title';
+    const newAuthor = 'updated author';
+
+    const editButton = screen.getAllByTestId('edit');
+    fireEvent.click(editButton[0]);
+    const input2 = screen.getByLabelText('Title');
+    const input3 = screen.getByLabelText('Author');
+
+    fireEvent.change(input2, { target: { value: newTitle } });
+    fireEvent.change(input3, { target: { value: newAuthor } });
+
+    await waitFor(() => {
+      fireEvent.submit(screen.getByText('決定'));
+    });
+    expect(screen.getByText(`Edit ${newTitle}`)).not.toBe(null);
+  });
+
+  test('記事の削除ができる', async () => {
+    const deleteButton = screen.getAllByTestId('delete');
+    fireEvent.click(deleteButton[0]);
+    fireEvent.click(screen.getByText('決定'));
+
+    await waitFor(() => {
+      expect(screen.getByText('Deleted.')).not.toBe(null);
+    });
+  });
+});
